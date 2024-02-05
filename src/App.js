ed978ccc069b601fbe14f9702eb4ed72952b2b5f
Karpe07 <karpeakash507@gmail.com>
2024-02-05 11:23:18 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+// Components
+import Messengers from './components/Messengers';
+import AccountProvider from './components/context/AccountProvider';
+
 
 function App() {
+  const clientId = '229491494917-sor829ase7mesrars70j2d32ai9tbrjm.apps.googleusercontent.com';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GoogleOAuthProvider clientId={clientId}>
+      <AccountProvider>
+        <Messengers />
+      </AccountProvider>
+    </GoogleOAuthProvider>
   );
 }
 
